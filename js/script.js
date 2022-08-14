a6b9a252732a6a544405bdffd8628f0808ab3dc8
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-14 19:36:01 +0200

    Vytvoril som addEL na section.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ let useSection = document.querySelectorAll("section");
 
 // Vypísanie section
 useSection.forEach( function( section ) {
+   section.addEventListener("click", function() {
     
+   }) 
 } )
 
