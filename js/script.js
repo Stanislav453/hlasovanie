d10d88173780c0ca340fb59595c9113d725da073
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-08-17 15:43:45 +0200

    Vymazal som celý kód a začínam odznova aby som pracval len s jedným objektom.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,2 @@
 "use strict";
 
-// Chytenie section
-let useSection = document.querySelectorAll("section");
-
-
-// Vypísanie section
-useSection.forEach( function( section ) {
-   section.addEventListener("click", function( event ) {
-     let score = Number( section.firstElementChild.textContent);
-      
-
-
-       section.firstElementChild.textContent = ++score
-        
-
- 
-
-     
-   }) ;
-} );
-
